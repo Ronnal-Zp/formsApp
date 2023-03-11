1d01d0817e1c56404c9d22c5e4962e07dc4fb593
“Aldahir” <ronn.zp.3@gmail.com>
2023-03-11 00:09:10 -0500

    rutas y primeros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { TemplateModule } from './template/template.module';
+import { ReactivesModule } from './reactive/reactives.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    TemplateModule,
+    ReactivesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
