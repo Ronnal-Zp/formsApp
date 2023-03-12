29372367ba3645e0e8489d51b88a64113570248f
Aldahir Z <ronn.zp.3@gmail.com>
2023-03-11 22:50:11 -0500

    template - validacion formulario basico

--- src/app/template/components/basicos/basicos.component.ts
+++ src/app/template/components/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./basicos.component.scss']
 })
 export class BasicosComponent {
+
+  @ViewChild('myForm') myForm!: NgForm;
+
+  enviar() {
+    console.log(this.myForm);
+    
+    this.myForm.resetForm({
+      nombre: 'Ninguno',
+      precio: 0,
+      existencia: 0
+    });
+  }
+
+  isValidNombre(): boolean {
+    return this.myForm?.controls['nombre']?.invalid && 
+           this.myForm?.controls['nombre']?.touched
+  }
+
+  isValidPrecio(): boolean {
+    return this.myForm?.controls['precio']?.touched &&
+           (this.myForm?.controls['precio']?.value < 0 || !this.myForm?.controls['precio']?.value)
+  }
+
+  isValidExistencia(): boolean {
+    return this.myForm?.controls['existencia']?.touched && this.myForm?.controls['existencia']?.invalid
+  }
 
 }
