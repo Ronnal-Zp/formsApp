5824de968ffe14a7e253f40b92ef304e12360e93
Aldahir Z <ronn.zp.3@gmail.com>
2023-03-12 00:26:27 -0500

    template - agregar formularios dinamicos

--- src/app/template/components/dinamicos/dinamicos.component.ts
+++ src/app/template/components/dinamicos/dinamicos.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,5 +17,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./dinamicos.component.scss']
 })
 export class DinamicosComponent {
+
+  @ViewChild('myForm') myForm!: NgForm;
+  nuevoFavorito: string = '';
+
+  persona: Persona = {
+    nombre: "Aldahir Zamora",
+    favoritos: [
+      { id: 1, nombre: 'God of War'},
+      { id: 2, nombre: 'GTA San Andreas' }
+    ]
+  }
+
+
+  /**
+   * Post del formulario
+   */
+  enviar() {
+    console.log('Formulario posteado');
+    console.log(this.myForm.value);
+  }
+
+  /**
+   * Agregar favorito
+   */
+  agregarFavorito() {
+    const favorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoFavorito
+    }
+
+    this.persona.favoritos.push( {...favorito} )
+    this.nuevoFavorito = '';
+  }
+
+  /**
+   * Elimiar favorito
+   * @param index 
+   */
+  elimiarFavorito(index: number) {
+     this.persona.favoritos.splice(index, 1);
+  }
+
+  /**
+   * Validar campo persona
+   * @returns 
+   */
+  isValidPersona(): boolean {
+    return this.myForm?.controls['persona']?.invalid && this.myForm?.controls['persona']?.touched
+  }
 
 }
