40c8f47414bca6b7b0a97f197b48ae7f2096a253
Aldahir Zamora <ronn.zp.3@gmail.com>
2024-09-08 17:25:31 -0500

    form basic, validaciones

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,50 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
 export class BasicPageComponent {
+
+  public form: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+
+  public isValidField(field: string): boolean | null {
+    return this.form.controls[field].errors && this.form.controls[field].touched
+  }
+
+  public getMessageError(field: string): string {
+    if( !this.form.controls[field] ) return '';
+
+    const errors = this.form.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido.';
+
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracteres.`;
+      }
+    }
+
+    return '';
+  }
+
+  public onSave(): void {
+    if(this.form.invalid) return;
+
+    console.log(this.form.value);
+
+    this.form.reset({price: 0, inStorage: 0})
+  }
+
 
 }
