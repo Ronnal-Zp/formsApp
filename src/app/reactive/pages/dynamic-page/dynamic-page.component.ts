855ead3f2a542f39bcf9c08acd93b75feb805279
Aldahir Zamora <ronn.zp.3@gmail.com>
2024-09-08 20:56:16 -0500

    avances form array

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,64 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
   styles: ``
 })
 export class DynamicPageComponent {
+
+  public form: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    favoriteGames: this.fb.array([
+      ['PvZ Warfare', [ Validators.required ]],
+      ['PUBG', [ Validators.required ]]
+    ])
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  public onSubmit() {
+    console.log(this.form.errors);
+    console.log(this.form.invalid)
+
+    if(this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.form.value);
+
+    this.form.reset();
+  }
+
+  get favoriteGames() {
+    return this.form.controls['favoriteGames'] as FormArray;
+  }
+
+  public isValidField(field: string): boolean | null {
+    return this.form.controls[field].errors && this.form.controls[field].touched
+  }
+
+  public isValidFieldInArray(formArray: FormArray, index: number): boolean | null {
+    return formArray.controls[index].errors && formArray.controls[index].touched
+  }
+
+  public getMessageError(field: string): string {
+    if( !this.form.controls[field] ) return '';
+
+    const errors = this.form.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido.';
+
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracteres.`;
+      }
+    }
+
+    return '';
+  }
 
 }
