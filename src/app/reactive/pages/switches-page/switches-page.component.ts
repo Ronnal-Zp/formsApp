3d92807303a380dbf4a12b576883d81af167aefe
Aldahir Zamora <ronn.zp.3@gmail.com>
2024-09-08 22:13:44 -0500

    switch page terminado

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
 export class SwitchesPageComponent {
+
+  public form: FormGroup = this.fb.group({
+    gender: ['M', [Validators.required]],
+    wantNotidications: [true, [Validators.required]],
+    termsAndConditions: [false, [Validators.requiredTrue]]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  public onSubmit(): void {
+    if(this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.form.value);
+  }
+
+  public isValidField(field: string): boolean | null {
+    return this.form.controls[field].errors && this.form.controls[field].dirty
+  }
 
 }
